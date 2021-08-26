b6159c243691f4c3431ee51f26d2404c6ea2bc76
Alex <alexamuek@gmail.com>
2021-08-26 19:12:29 +0800

    test fixes

--- tests/long_test_wrapper_unWrap721_2.py
+++ tests/long_test_wrapper_unWrap721_2.py
@@ -18,21 +18,22 @@ royaltyBeneficiary = '0xbd7e5fb7525ed8583893ce1b1f93e21cc0cf02f6'
 zero_address = '0x0000000000000000000000000000000000000000'
 
 #unwrap 
-def test_wrapper_unWrap721_batch(accounts, erc721mock, wrapper, niftsy20, dai, weth, TokenMock):
+def test_wrapper_unWrap721_batch(accounts, erc721mock, wrapper, niftsy20, dai, weth, TokenMock, trmodel):
 	#make test data
 	makeNFTForTest(accounts, erc721mock, ORIGINAL_NFT_IDs)
-	wrapper.setFee(protokolFee, chargeFeeAfter, {"from": accounts[0]})
+	wrapper.setFee(protokolFee, chargeFeeAfter, niftsy20,{"from": accounts[0]})
 	erc721mock.approve(wrapper.address, ORIGINAL_NFT_IDs[0], {'from':accounts[1]})
 	logging.info('balanceOf(wrapper.address) = {}'.format(niftsy20.balanceOf(wrapper.address)))
 	logging.info('balanceOf(royaltyBeneficiary) = {}'.format(niftsy20.balanceOf(royaltyBeneficiary)))
 	#wrap difficult nft
 
 	niftsy20.transfer(accounts[1], protokolFee, {"from": accounts[0]})
+	niftsy20.approve(wrapper, protokolFee+TRANSFER_FEE, {'from':accounts[1]})
 	unwrapAfter = chain.time() + 10
 	
+
 	before_balance = niftsy20.balanceOf(wrapper.address) 
-	niftsy20.approve(wrapper, TRANSFER_FEE, {'from': accounts[1]})
-	makeWrapNFT(wrapper, erc721mock, ['originalTokenId'], [ORIGINAL_NFT_IDs[0]], accounts[1])
+	makeWrapNFT(wrapper, erc721mock, ['originalTokenId'], [ORIGINAL_NFT_IDs[0]], accounts[1], niftsy20)
 	assert niftsy20.balanceOf(accounts[1]) == 0
 	assert niftsy20.balanceOf(wrapper.address) == before_balance + protokolFee
 	assert wrapper.lastWrappedNFTId() == 1
@@ -40,14 +41,15 @@ def test_wrapper_unWrap721_batch(accounts, erc721mock, wrapper, niftsy20, dai, w
 	tokenId = wrapper.lastWrappedNFTId()
 	nft = wrapper.getWrappedToken(tokenId)
 
-	c = round(UNWRAP_FEE_THRESHOLD/TRANSFER_FEE) + 1
+	c = round(UNWRAP_FEE_THRESHOLD/(TRANSFER_FEE*(100-ROAYLTY_PERCENT)/100)) + 1
 	backedTokens = 0
 	royalty_tokens = 0
 	i = 0
 	for i in range(c):
 		wrapper.approve(accounts[i+2].address, tokenId, {"from": accounts[i+1]})
+		niftsy20.approve(trmodel.address, TRANSFER_FEE, {'from': accounts[i+1]})
+		logging.info('niftsy20.allowance(accounts[i+1], trmodel.address) = {}'.format(niftsy20.allowance(accounts[i+1], trmodel.address)))
 		niftsy20.transfer(accounts[i + 1].address, TRANSFER_FEE, {"from": accounts[0]}) #add niftsy tokens to pay transfer fee
-		niftsy20.approve(wrapper, TRANSFER_FEE, {'from': accounts[i+1]})
 		wrapper.transferFrom(accounts[i+1].address, accounts[i+2].address, tokenId, {"from": accounts[i+2]}) #make transfers
 		royalty_tokens += TRANSFER_FEE*nft[7]/100
 		backedTokens += TRANSFER_FEE - TRANSFER_FEE*nft[7]/100
